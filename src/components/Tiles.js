2e433f42f247c1b4c0541790ea64595ac9ad6cc3
jnielsen85 <thisisjn@gmail.com>
2020-07-10 16:37:52 +1000

    divs for game board added

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -3,16 +3,14 @@ import React, { Component } from 'react'
 class Tiles extends Component {
   render() {
     return (
-      <div>
-        <tbody>
-          <tr>
-            <td class="square">&nbsp;</td>
-            <td class="square">&nbsp;</td>
-            <td class="square">&nbsp;</td>
-            <td class="square">&nbsp;</td>
-            <td class="square">&nbsp;</td>
-          </tr>
-        </tbody>
+      <div class="board" >
+          <div class="tiles">tiles</div>
+          <div class="tiles">tiles</div>
+          <div class="tiles">tiles</div>
+          <div class="tiles">tiles</div>
+          <div class="tiles">tiles</div>
+          <div class="tiles">tiles</div>
+          <div class="tiles">tiles</div>
       </div>
     )
   }
