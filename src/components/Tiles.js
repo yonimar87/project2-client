53b09ef1287ef7362599a9486751e8547220c1a1
yonathan marlow <yonathanmarlow@hotmail.com>
2020-07-13 11:14:45 +1000

    new location for the drink

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
-import Shotglass from './Shotglass'
+import shotglass from "../images/shotglass.jpg"; // with import
+
 
 
 class Tiles extends Component {
+  // constructor(props){
+  //   super(props)
+
+  // const drinkUrl = url(../images/shotglass.jpg)
+  // const market = 4
+  //
+  //   then in the state do an
+  //   if  (heads) = true
+  //     setvalue = false for the one im on // image disappears from current div.
+  //      marker -1
+  //     setState value = true
+  //     tile to left tile to right
+  //     else {
+  //       setvalue = false for the one im on
+  //       marker + 1
+  //       setstate value = true
+  //       to the other side.
+
   render() {
     return (
       <div class="board" >
+          <h1></h1>
           <div class="tiles" id="tile1">1</div>
           <div class="tiles" id="tile2">2</div>
           <div class="tiles" id="tile3">3</div>
@@ -17,6 +37,15 @@ class Tiles extends Component {
     )
   }
 }
+
+class Shotglass extends Component {
+  render() {
+    return <div>
+      <img id="shotglass" src={shotglass}/>
+    </div>
+  }
+}
+
 export default Tiles
 
 {/*
