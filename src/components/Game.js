546ab1cf59932f226cb383ab7f8b08d430ecf63e
jnielsen85 <thisisjn@gmail.com>
2020-07-16 20:14:08 +1000

    counter class removed

--- src/components/Game.js
+++ src/components/Game.js
@@ -217,34 +217,5 @@ class Shotglass extends Component {
   }
 }
 
-//------- child ----------------------------------
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 0
-    }
-  }
-  win = () => {
-    this.setState({
-      player1Wins: this.state.count + 1
-    })
-  }
-  loss = () => {
-    this.setState({
-      count: this.state.count - 1
-    })
-  }
-  render() {
-    return (
-      <div className="playerpoints">
-        <h1 className="playername"> Player {} </h1>
-        <p className="pointstotal"> Total Points: {this.state.count} </p>
-        <button onClick={this.win}> Player 1 wins: </button>
-        <button onClick={this.loss}> Player 2 wins: </button>
-      </div>
-    )
-  }
-}
 //---------child-------------------------------
 export default Game
