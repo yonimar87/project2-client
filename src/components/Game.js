04567155990ca27e11df45d912396c8fd9b54922
jnielsen85 <thisisjn@gmail.com>
2020-07-16 13:23:54 +1000

    gameplay and css updated

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,23 +6,24 @@ class Game extends Component {
     this.state = {
       result: '',
       Winner: '',
+      displayWinner : false,
       turnCounter: 0,
       tilesSize: 7,
       marker: 3,
-      tiles: [false, false,false,true,false,false, false]
+      tiles: [false, false,false,true,false,false,false],
+      player1Wins: 0,
+      player2Wins: 0
     }
     this.coinToss = this.coinToss.bind(this)
     this.buildArray = this.buildArray.bind(this)
     this.generateTiles = this.generateTiles.bind(this)
     this._handleClick = this._handleClick.bind(this)
-
   }
-
   coinToss() {
     this.setState({result: ''});
     setTimeout( () => {
       let tossOutcome = '';
-      if (Math.random() < 0.7) {
+      if (Math.random() < 0.5) {
         tossOutcome = "heads";
         //this.setState({ result: "heads" }, this.buildArray);
         console.log(tossOutcome);
@@ -34,10 +35,11 @@ class Game extends Component {
       this.setState({result: tossOutcome}, this.buildArray);
     },0);
   }
-
   buildArray() {
     let tCounter = this.state.turnCounter;
     let tMarker = this.state.marker;
+    let tplayer1wins = this.state.player1Wins;
+    let tplayer2wins = this.state.player2Wins;
     let player1 = 6;
     let player2 = 0;
     let winner = '';
@@ -54,9 +56,13 @@ class Game extends Component {
         //alert("Congratulation Player 2 wins! ")
       }
     }
-    this.setState({marker: tMarker, turnCounter: tCounter +1, Winner: winner}, this.generateTiles)
+      this.setState({marker: tMarker, turnCounter: tCounter +1, Winner: winner}, this.generateTiles);
+      if (winner) {
+        setTimeout(() => {
+          this.setState({displayWinner: true, player1Wins: tplayer1wins, player2Wins: tplayer2wins})
+        }, 3000)
+      }    console.log(this.state.player1Wins, this.state.player2Wins);
   }
-
   generateTiles() {
     const arrayTiles = []
     setTimeout( () => {
@@ -72,32 +78,27 @@ class Game extends Component {
     //console.log(this.state.tiles)
   },2050);
   }
-
   _handleClick(event) {
-    this.setState({turnCounter: 0, marker: 3, result: '', Winner: '', tiles: [false, false,false,true,false,false, false]})
+    this.setState({turnCounter: 0, marker: 3, result: '', Winner: '', displayWinner: false, tiles: [false, false,false,true,false,false, false]})
   }
-
   render() {
     return (
       <div className="gamePage">
         <div className="p1">
-          <h1>Player 1</h1>
+          <h1>Player 1: {this.state.player1Wins}</h1>
         </div>
         <div className="flip">
           <Coinflip coinFlip={this.coinToss} outcome={this.state.result} />
         </div>
         <div className="p2">
-          <h1>Player 2</h1>
+          <h1>Player 2: {this.state.player2Wins}</h1>
+        </div>
+        <div className="spacer">
         </div>
         <div className="gameBottom">
             <TileSet tiles={this.state.tiles} />
         </div>
-        <div className="tilesParent">
-          { /* <TileSet /> */ }
-        </div>
-
-        { this.state.Winner && <WinDiv Winner={this.state.Winner} _handleClick={this._handleClick} /> }
-
+        { this.state.displayWinner && <WinDiv Winner={this.state.Winner} _handleClick={this._handleClick} /> }
       </div>
     )
   }
@@ -144,22 +145,10 @@ class Coinflip extends React.Component {
   }
 }
 //---------------child -----------------------
-
 const WinDiv = (props) => <div className="winner">
   <div className="drink"> {props.Winner} Drink!!</div>
   <div className="restart" onClick={props._handleClick} >Play Again</div>
   </div>
-
-// class WinDiv extends React.Component {
-//   render () {
-//     return (
-//       <div>
-//         {this.props.Winner}
-//       </div>
-//     )
-//   }
-// }
-//
 
 //-----------childs-------------------------------
 class Shotglass extends Component {
@@ -173,26 +162,22 @@ class Shotglass extends Component {
 }
  //------- child ----------------------------------
 class Counter extends Component {
-
 constructor(props) {
   super(props);
   this.state = {
     count: 0
   };
 }
-
 win = () => {
   this.setState({
-    count: this.state.count + 1
+    player1Wins: this.state.count + 1
   })
 };
-
 loss = () => {
   this.setState({
     count: this.state.count - 1
   })
 };
-
   render() {
     return (
     <div className="playerpoints">
@@ -204,5 +189,5 @@ loss = () => {
     );
   }
 }
-
+//---------child-------------------------------
 export default Game
